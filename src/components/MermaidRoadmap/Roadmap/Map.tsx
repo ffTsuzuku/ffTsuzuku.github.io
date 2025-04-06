4ee5fd5a44af6f033a714a07d8e4c544455ddd9b
Deep <gagandeepsandhu@protonmail.com>
2025-04-06 18:33:33 -0400

    replaced svg with zoomable roadmap

--- src/components/MermaidRoadmap/Roadmap/Map.tsx
+++ src/components/MermaidRoadmap/Roadmap/Map.tsx
@@ -1,21 +1,24 @@
 import { Flex, Image, useColorModeValue } from "@chakra-ui/react";
 import darkmap from "../../../imgs/roadmap/darkmap.svg";
 import lightmap from "../../../imgs/roadmap/lightmap.svg";
+import { MapInteractionCSS } from "react-map-interaction";
+
 const Map = () => {
-	const img = useColorModeValue(lightmap, darkmap)
-	const bg = useColorModeValue('#edf2f6', '#2c313d')
+  const img = useColorModeValue(lightmap, darkmap);
+  const bg = useColorModeValue("#edf2f6", "#2c313d");
   return (
-    <Flex
-			backgroundColor={bg}
-      w={"90%"}
-      ml={"auto"}
-      mr={"auto"}
-      height={"400px"}
-      borderRadius={"md"}
-			p={'20px'}
-      shadow={"lg"}
-    >
-      <Image src={img} w={"100%"} borderRadius={"md"} />
+    <Flex w="90%" bgColor={bg} borderRadius={"md"} ml={"auto"} mr={"auto"} p="20px">
+      <MapInteractionCSS
+        minScale={0.5}
+        maxScale={3}
+        translationBounds={{
+          xMax: 400,
+          yMax: 200,
+        }}
+        showControls
+      >
+        <Image src={img} height={"400px"} />
+      </MapInteractionCSS>
     </Flex>
   );
 };
